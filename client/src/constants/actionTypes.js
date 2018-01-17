9fa4617100fa4097c9e411ebc890b414742bcdfa
nhatlinhdoan <mnhatlinh.doan@gmail.com>
2018-01-17 20:23:43 +0700

    a

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by linhndoan on 12/17/2017.
+ */
+
+//ACCOUNT
+export const RECEIVE_POST_LOGIN_TODO = 'LOGIN_TODO';
+export const RECEIVE_POST_REGISTER_TODO =  'REGISTER_TODO';
+
+//DEFAULT
+export const REQUEST_POST_TODO = 'REQUEST_POST_TODO';
+export const LOG_OUT_TODO = 'LOG_OUT_TODO';
